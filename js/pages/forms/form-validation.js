9f6ef5413fafa467e90382b61242940e9081c1de
edwardchai <robin.chai@yahoo.com>
2019-04-13 22:59:08 +0800

    Update_2019.04.13_02

--- js/pages/forms/form-validation.js
+++ js/pages/forms/form-validation.js
@@ -49,8 +49,28 @@ $(function () {
     });
 
     $('#add_RP').validate({
+        // rules: {
+        //     'rpName': {
+        //         required: true
+        //     }
+        // },
+        highlight: function (input) {
+            $(input).parents('.form-line').addClass('error');
+        },
+        unhighlight: function (input) {
+            $(input).parents('.form-line').removeClass('error');
+        },
+        errorPlacement: function (error, element) {
+            $(element).parents('.form-group').append(error);
+        }
+    });
+
+    $('#add_job').validate({
         rules: {
-            'rpName': {
+            // 'jobName': {
+            //     required: true
+            // },
+            'specificity': {
                 required: true
             }
         },
